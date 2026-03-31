39617f66304af44490bff06b97fcaacaf89bf9e4
Dmitriy Afanasyev <dmitriy-afanasyev@users.noreply.github.com>
2026-03-31 15:59:21 +0300

    fix: format

--- after/01-ddd/bootstrap/app.php
+++ after/01-ddd/bootstrap/app.php
@@ -8,20 +8,20 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-    web: __DIR__ . '/../routes/web.php',
-    commands: __DIR__ . '/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withProviders([
-    GeocoderServiceProvider::class,
-])
+        GeocoderServiceProvider::class,
+    ])
     ->withMiddleware(function (Middleware $middleware): void {
         //
     })
     ->withExceptions(
         function (Exceptions $exceptions): void {
-        $exceptions->render(function (\Throwable $e) {
-            return GeocoderExceptionHandler::handle($e);
+            $exceptions->render(function (\Throwable $e) {
+                return GeocoderExceptionHandler::handle($e);
             });
         }
     )->create();
